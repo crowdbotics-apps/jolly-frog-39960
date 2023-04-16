c9b562febe512763e3baddde28b401c4be680e50
Samantha Chapman <samchap770@gmail.com>
2023-04-15 20:40:19 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "jolly-frog-39960",
   url: "https://jolly-frog-39960.botics.co",
   api: "https://jolly-frog-39960.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
